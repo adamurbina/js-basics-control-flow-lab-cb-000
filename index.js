12675f989ccdc1b72797978efcd1e5ba1aa63c1f
Adam Urbina <adamurbina@gmail.com>
2019-01-12 02:23:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function scuberGreetingForFeet(ride){
     case ride > 2500:
       response = 'No can do.'
       break;
-    case ride <= 400:
-      response = 'This one is on me!';
-      break;
     case ride > 2000:
       response = 'I will gladly take your thirty bucks.'
+      break;
+    case ride <= 400:
+      response = 'This one is on me!';
+      break;  
   }
   return response;
 }
