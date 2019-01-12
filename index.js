c25107a390ae85cfdf6b00adbbcab3b0ef16206e
Adam Urbina <adamurbina@gmail.com>
2019-01-12 02:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(someValue){
-  
+function scuberGreetingForFeet(ride){
+  let response;
+
+  switch (true) {
+    case ride <= 400:
+      response = 'This one is on me!';
+      break;
+    case ride > 3000:
+      response = 'I will gladly take your thirty bucks.'
+    default:
+
+  }
 }
